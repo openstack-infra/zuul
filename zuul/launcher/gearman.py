blurrycontour/zuul-gerrit

Allow workers to send back metadata
To help with debugging, allow workers to send back information about
itself such as hostname and ips. This will allow for an RPC/Client
call to get the information on running jobs.

Change-Id: I03b553293923cd65b3a9651a8877aa23688d2909

zuul/launcher/gearman.py
@@ -381,6 +381,8 @@ class Gearman(object):
         if build:
             # Allow URL to be updated
             build.url = data.get('url') or build.url
+            # Update information about worker
+            build.worker.update_from_data(data)
 
             if build.number is None:
                 self.log.info("Build %s started" % job)

openstack-infra/zuul

zuul/launcher/gearman.py
@@ -381,6 +381,8 @@ class Gearman(object):
         if build:
             # Allow URL to be updated
             build.url = data.get('url') or build.url
+            # Update information about worker
+            build.worker.updateFromData(data)
 
             if build.number is None:
                 self.log.info("Build %s started" % job)