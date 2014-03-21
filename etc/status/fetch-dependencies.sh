4863b60280db05e235960119a1caf925cd1c7c95
Joshua Hesketh <josh@nitrotech.org>
2014-03-21 14:19:06 +1100

    Update zuul's status page to bootstrap 3.1.1
    Also utilise bootstrap's components more for display.
    
    Change-Id: I7c89a5cda2cdb9f4b3e1d473f7d7792840e9fd61

--- etc/status/fetch-dependencies.sh
+++ etc/status/fetch-dependencies.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 BASE_DIR=$(cd $(dirname $0); pwd)
 echo "Destination: $BASE_DIR/public_html"
+
 echo "Fetching jquery.min.js..."
 curl --silent http://code.jquery.com/jquery.min.js > $BASE_DIR/public_html/jquery.min.js
+
 echo "Fetching jquery-visibility.min.js..."
 curl --silent https://raw.github.com/mathiasbynens/jquery-visibility/master/jquery-visibility.min.js > $BASE_DIR/public_html/jquery-visibility.min.js
+
 echo "Fetching bootstrap..."
-curl --silent http://twitter.github.io/bootstrap/assets/bootstrap.zip > bootstrap.zip && unzip -q -o bootstrap.zip -d $BASE_DIR/public_html/ && rm bootstrap.zip
+curl -L --silent https://github.com/twbs/bootstrap/releases/download/v3.1.1/bootstrap-3.1.1-dist.zip > bootstrap.zip
+unzip -q -o bootstrap.zip -d $BASE_DIR/public_html/
+mv $BASE_DIR/public_html/bootstrap-3.1.1-dist $BASE_DIR/public_html/bootstrap
+rm bootstrap.zip
