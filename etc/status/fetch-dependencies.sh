blurrycontour/zuul-gerrit

Fix up status page dependencies
bootstrap-responsive is no longer required.
Make sure we follow redirects when fetching deps

Change-Id: Ib1222fcb590def5c0147a53f046def86da745830

etc/status/fetch-dependencies.sh
@@ -3,10 +3,10 @@ BASE_DIR=$(cd $(dirname $0); pwd)
 echo "Destination: $BASE_DIR/public_html"
 
 echo "Fetching jquery.min.js..."
-curl --silent http://code.jquery.com/jquery.min.js > $BASE_DIR/public_html/jquery.min.js
+curl -L --silent http://code.jquery.com/jquery.min.js > $BASE_DIR/public_html/jquery.min.js
 
 echo "Fetching jquery-visibility.min.js..."
-curl --silent https://raw.github.com/mathiasbynens/jquery-visibility/master/jquery-visibility.min.js > $BASE_DIR/public_html/jquery-visibility.min.js
+curl -L --silent https://raw.github.com/mathiasbynens/jquery-visibility/master/jquery-visibility.js > $BASE_DIR/public_html/jquery-visibility.js
 
 echo "Fetching jquery.graphite.js..."
 curl -L --silent https://github.com/prestontimmons/graphitejs/archive/master.zip > jquery-graphite.zip

openstack-infra/zuul

etc/status/fetch-dependencies.sh
@@ -3,10 +3,10 @@ BASE_DIR=$(cd $(dirname $0); pwd)
 echo "Destination: $BASE_DIR/public_html"
 
 echo "Fetching jquery.min.js..."
-curl --silent http://code.jquery.com/jquery.min.js > $BASE_DIR/public_html/jquery.min.js
+curl -L --silent http://code.jquery.com/jquery.min.js > $BASE_DIR/public_html/jquery.min.js
 
 echo "Fetching jquery-visibility.min.js..."
-curl --silent https://raw.githubusercontent.com/mathiasbynens/jquery-visibility/master/jquery-visibility.js > $BASE_DIR/public_html/jquery-visibility.min.js
+curl -L --silent https://raw.githubusercontent.com/mathiasbynens/jquery-visibility/master/jquery-visibility.js > $BASE_DIR/public_html/jquery-visibility.js
 
 echo "Fetching jquery.graphite.js..."
 curl -L --silent https://github.com/prestontimmons/graphitejs/archive/master.zip > jquery-graphite.zip