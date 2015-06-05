blurrycontour/zuul-gerrit

Add support for negative requirements
This change adds support for false matching of requirements. To make
this useful you can now require all approvals match a requirement or
only one (ie any).

Therefore depreciate require-approvals, replacing with
require-approvals-any and a new require-approvals-all.

This needs some more tests.
(Depends on German beer from Jogo)

Change-Id: I458e677315ccb90d64cd0c0e734951141324a9c3

zuul/layoutvalidator.py
@@ -59,6 +59,8 @@ class LayoutSchema(object):
                       'branch': toList(str),
                       'ref': toList(str),
                       'approval': toList(variable_dict),
+                      'require-approval-all': toList(require_approval),
+                      'require-approval-any': toList(require_approval),
                       'require-approval': toList(require_approval),
                       }
 
@@ -74,7 +76,9 @@ class LayoutSchema(object):
                                },
                       }
 
-    require = {'approval': toList(require_approval),
+    require = {'approval-all': toList(require_approval),
+               'approval-any': toList(require_approval),
+               'approval': toList(require_approval),
                'open': bool,
                'current-patchset': bool,
                'status': toList(str)}

openstack-infra/zuul

zuul/layoutvalidator.py
@@ -62,6 +62,8 @@ class LayoutSchema(object):
                       'branch': toList(str),
                       'ref': toList(str),
                       'approval': toList(variable_dict),
+                      'require-all-approvals': toList(require_approval),
+                      'require-any-approval': toList(require_approval),
                       'require-approval': toList(require_approval),
                       }
 
@@ -85,7 +87,9 @@ class LayoutSchema(object):
                                },
                       }
 
-    require = {'approval': toList(require_approval),
+    require = {'all-approvals': toList(require_approval),
+               'any-approval': toList(require_approval),
+               'approval': toList(require_approval),
                'open': bool,
                'current-patchset': bool,
                'status': toList(str)}