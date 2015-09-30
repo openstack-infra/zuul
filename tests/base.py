ff977bf7a74e001b42b59576d36ac7658766045f
Ian Wienand <iwienand@redhat.com>
2015-09-30 15:38:47 +1000

    Use 127.0.0.1 rather than localhost for statsd host
    When we use "localhost" as the statsd server, statsd can possibly
    choose ::1 (i.e. ipv6) and all stats tests fail because the fake stats
    server is ipv4.
    
    This seems more prevalent with python2.6 (failures in our centos6 CI,
    replicated with Debian stable), probably because ordering of results
    changes in later glibcs.
    
    Anyway, specify 127.0.0.1 so we're getting the right thing
    
    Change-Id: I2f821834ab4d7e06cdb5933b79b9f650d9668c0e

--- tests/base.py
+++ tests/base.py
@@ -923,7 +923,9 @@ class ZuulTestCase(BaseTestCase):
         self.init_repo("org/no-jobs-project")
 
         self.statsd = FakeStatsd()
-        os.environ['STATSD_HOST'] = 'localhost'
+        # note, use 127.0.0.1 rather than localhost to avoid getting ipv6
+        # see: https://github.com/jsocol/pystatsd/issues/61
+        os.environ['STATSD_HOST'] = '127.0.0.1'
         os.environ['STATSD_PORT'] = str(self.statsd.port)
         self.statsd.start()
         # the statsd client object is configured in the statsd module import
