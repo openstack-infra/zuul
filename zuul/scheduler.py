blurrycontour/zuul-gerrit

Add option to ignore ref-updated events emitted by branch deletions
When a branch is deleted, gerrit emits a ref-updated event with a newrev
value of all zeros. This adds a boolean field to optionally not trigger
on these ref-updated events.

Change-Id: Iea75d05ddcb49b0bf748b72b9d2d5472d077f0c6

zuul/scheduler.py
@@ -347,6 +347,7 @@ class Scheduler(threading.Thread):
                     usernames = toList(trigger.get('username'))
                     if not usernames:
                         usernames = toList(trigger.get('username_filter'))
+                    ignore_deletes = trigger.get('ignore-deletes', False)
                     f = EventFilter(
                         trigger=self.triggers['gerrit'],
                         types=toList(trigger['event']),
@@ -358,7 +359,8 @@ class Scheduler(threading.Thread):
                         usernames=usernames,
                         required_approvals=toList(
                             trigger.get('require-approval')
-                        )
+                        ),
+                        ignore_deletes=ignore_deletes
                     )
                     manager.event_filters.append(f)
             if 'timer' in conf_pipeline['trigger']:

openstack-infra/zuul

zuul/scheduler.py
@@ -347,6 +347,7 @@ class Scheduler(threading.Thread):
                     usernames = toList(trigger.get('username'))
                     if not usernames:
                         usernames = toList(trigger.get('username_filter'))
+                    ignore_deletes = trigger.get('ignore-deletes', True)
                     f = EventFilter(
                         trigger=self.triggers['gerrit'],
                         types=toList(trigger['event']),
@@ -358,7 +359,8 @@ class Scheduler(threading.Thread):
                         usernames=usernames,
                         required_approvals=toList(
                             trigger.get('require-approval')
-                        )
+                        ),
+                        ignore_deletes=ignore_deletes
                     )
                     manager.event_filters.append(f)
             if 'timer' in conf_pipeline['trigger']: