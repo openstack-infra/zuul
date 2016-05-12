blurrycontour/zuul-gerrit

Ansible launcher: add zuul_runner module
This runs the commands asynchronously (but waits for their
completion).  This is more robust for long-running commands
because it avoids the built-in ssh timeout.

This adds an ansible module to actually run the remote command
so that we can:
 * process the console log
 * use ansible async (the script module does not support it)
 * control the environment variables of the script being run

It also adds a callback plugin to track the elapsed time so that
we can use the built-in timeout features of async commands.

Note that the module and plugin are GPL licensed.

Change-Id: I19b2b6a5c362bb9d843e7802aefe0eb5df9c5ed7

zuul/ansible/library/zuul_runner.py
@@ -1,0 +1,75 @@
+#!/usr/bin/python
+
+# Copyright (c) 2016 IBM Corp.
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import select
+import subprocess
+
+
+class Console(object):
+    def __enter__(self):
+        self.logfile = open('/tmp/console.log', 'w+')
+        return self
+
+    def __exit__(self, etype, value, tb):
+        self.logfile.close()
+
+    def addLine(self, ln):
+        ts = datetime.datetime.now()
+        outln = '%s %s' % (str(ts), ln)
+        self.logfile.write(ln)
+
+
+def run(cwd, cmd, args):
+    proc = subprocess.Popen(
+        [cmd],
+        cwd=cwd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        env=args,
+    )
+
+    with Console() as console:
+        while True:
+            line = proc.stdout.readline()
+            if not line:
+                break
+            console.addLine(line)
+
+    ret = proc.wait()
+    return ret
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            command=dict(required=True, default=None),
+            cwd=dict(required=True, default=None),
+            parameters=dict(default={}, type='dict')
+        )
+    )
+
+    p = module.params
+    ret = run(p['cwd'], p['command'], p['parameters'])
+    if ret == 0:
+        module.exit_json(changed=True, rc=ret)
+    else:
+        module.fail_json(msg="Exit code %s" % ret, rc=ret)
+
+from ansible.module_utils.basic import *
+
+main()

openstack-infra/zuul

zuul/ansible/library/zuul_runner.py
@@ -1,0 +1,74 @@
+#!/usr/bin/python
+
+# Copyright (c) 2016 IBM Corp.
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import subprocess
+
+
+class Console(object):
+    def __enter__(self):
+        self.logfile = open('/tmp/console.log', 'w+')
+        return self
+
+    def __exit__(self, etype, value, tb):
+        self.logfile.close()
+
+    def addLine(self, ln):
+        ts = datetime.datetime.now()
+        outln = '%s %s' % (str(ts), ln)
+        self.logfile.write(outln)
+
+
+def run(cwd, cmd, args):
+    proc = subprocess.Popen(
+        [cmd],
+        cwd=cwd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        env=args,
+    )
+
+    with Console() as console:
+        while True:
+            line = proc.stdout.readline()
+            if not line:
+                break
+            console.addLine(line)
+
+    ret = proc.wait()
+    return ret
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            command=dict(required=True, default=None),
+            cwd=dict(required=True, default=None),
+            parameters=dict(default={}, type='dict')
+        )
+    )
+
+    p = module.params
+    ret = run(p['cwd'], p['command'], p['parameters'])
+    if ret == 0:
+        module.exit_json(changed=True, rc=ret)
+    else:
+        module.fail_json(msg="Exit code %s" % ret, rc=ret)
+
+from ansible.module_utils.basic import *  # noqa
+
+main()