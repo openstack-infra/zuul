e77bf87fd4a00150feec13f783cc4bf0c5987625
Morgan Fainberg <morgan.fainberg@gmail.com>
2016-05-30 14:31:08 -0700

    Python 3 Fixes: use six.moves.configparser
    Use six.moves.configparser instead of ConfigParser for python3 compat.
    
    Change-Id: Ib96e3f1f6cb6959529b3982209e7998ec14d76f5

--- tests/test_layoutvalidator.py
+++ tests/test_layoutvalidator.py
@@ -14,7 +14,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import ConfigParser
+from six.moves import configparser as ConfigParser
 import os
 import re
 
