0d9521568441e46a47e78037b4906d5305b17364
James E. Blair <jeblair@redhat.com>
2017-02-07 17:14:44 -0800

    Implement node equality
    When comparing jobs, nodesets are compared, which includes the list
    of nodes.  However, equality on the node class itself was not implemented
    meaning that any job which actually specified nodes would not evaluate
    as equal to itself.  Correct that.
    
    Change-Id: Iba04c7377783396105961b0a0ea55651aacc17b6

--- zuul/model.py
+++ zuul/model.py
@@ -404,6 +404,16 @@ class Node(object):
     def __repr__(self):
         return '<Node %s %s:%s>' % (self.id, self.name, self.image)
 
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            return False
+        return (self.name == other.name and
+                self.image == other.image and
+                self.id == other.id)
+
     def toDict(self):
         d = {}
         d['state'] = self.state
