e3162026294887c12ba60b3f21cfadb22bc7cef1
James E. Blair <jeblair@redhat.com>
2017-02-20 16:47:27 -0500

    Fix branch-based caching of unparsed config
    This fixes two problems:
    
    * We were using an UnparsedTenantConfig object to cache the previously
      parsed config for use in reconfigurations (static or dynamic).  But
      in the dynamic config path, we would end up passing that back into
      the yaml parser.  Skip that step.
    * That cached data was for the whole project, but we are assembling
      the config one project-branch at a time.  Since we have other areas
      that use the full project config, keep that, but also cache the
      UnparsedTenantConfig for each project-branch.
    
    Change-Id: Ie3c95cb2236b517d28bec45590c9eea6c1802008

--- zuul/model.py
+++ zuul/model.py
@@ -365,6 +365,7 @@ class Project(object):
         # TODOv3 (jeblair): re-add support for foreign projects if needed
         self.foreign = foreign
         self.unparsed_config = None
+        self.unparsed_branch_config = {}  # branch -> UnparsedTenantConfig
 
     def __str__(self):
         return self.name
