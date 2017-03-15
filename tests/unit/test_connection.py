blurrycontour/zuul-gerrit

Rename zuul-launcher to zuul-executor
To avoid confusion with nodepool-launcher, we've decided to rename
zuul-launcher to zuul-executor.

Change-Id: I7d03cf0f0093400f4ba2e4beb1c92694224a3e8c
Signed-off-by: Paul Belanger <pabelanger@redhat.com>

tests/unit/test_connection.py
@@ -46,7 +46,7 @@ class TestConnections(ZuulTestCase):
                          'jenkins')
 
         B = self.fake_review_gerrit.addFakeChange('org/project', 'master', 'B')
-        self.launch_server.failJob('project-test2', B)
+        self.execute_server.failJob('project-test2', B)
         self.addEvent('review_gerrit', B.getPatchsetCreatedEvent(1))
 
         self.waitUntilSettled()
@@ -239,7 +239,7 @@ class TestMultipleGerrits(ZuulTestCase):
     tenant_config_file = 'config/zuul-connections-multiple-gerrits/main.yaml'
 
     def test_multiple_project_separate_gerrits(self):
-        self.launch_server.hold_jobs_in_build = True
+        self.execute_server.hold_jobs_in_build = True
 
         A = self.fake_another_gerrit.addFakeChange(
             'org/project1', 'master', 'A')
@@ -276,6 +276,6 @@ class TestMultipleGerrits(ZuulTestCase):
                  pipeline='review_check'),
         ])
 
-        self.launch_server.hold_jobs_in_build = False
-        self.launch_server.release()
+        self.execute_server.hold_jobs_in_build = False
+        self.execute_server.release()
         self.waitUntilSettled()

tests/unit/test_openstack.py
@@ -58,7 +58,7 @@ class TestOpenStack(AnsibleZuulTestCase):
                          'ubuntu-trusty')
 
     def test_dsvm_keystone_repo(self):
-        self.launch_server.keep_jobdir = True
+        self.execute_server.keep_jobdir = True
         A = self.fake_gerrit.addFakeChange('openstack/nova', 'master', 'A')
         self.fake_gerrit.addEvent(A.getPatchsetCreatedEvent(1))
         self.waitUntilSettled()
@@ -68,9 +68,9 @@ class TestOpenStack(AnsibleZuulTestCase):
         build = self.getJobFromHistory('dsvm')
 
         # Check that a change to nova triggered a keystone clone
-        launcher_git_dir = os.path.join(self.launcher_src_root,
+        executor_git_dir = os.path.join(self.executor_src_root,
                                         'openstack', 'keystone', '.git')
-        self.assertTrue(os.path.exists(launcher_git_dir),
+        self.assertTrue(os.path.exists(executor_git_dir),
                         msg='openstack/keystone should be cloned.')
 
         jobdir_git_dir = os.path.join(build.jobdir.src_root,
@@ -79,7 +79,7 @@ class TestOpenStack(AnsibleZuulTestCase):
                         msg='openstack/keystone should be cloned.')
 
     def test_dsvm_nova_repo(self):
-        self.launch_server.keep_jobdir = True
+        self.execute_server.keep_jobdir = True
         A = self.fake_gerrit.addFakeChange('openstack/keystone', 'master', 'A')
         self.fake_gerrit.addEvent(A.getPatchsetCreatedEvent(1))
         self.waitUntilSettled()
@@ -89,9 +89,9 @@ class TestOpenStack(AnsibleZuulTestCase):
         build = self.getJobFromHistory('dsvm')
 
         # Check that a change to keystone triggered a nova clone
-        launcher_git_dir = os.path.join(self.launcher_src_root,
+        executor_git_dir = os.path.join(self.executor_src_root,
                                         'openstack', 'nova', '.git')
-        self.assertTrue(os.path.exists(launcher_git_dir),
+        self.assertTrue(os.path.exists(executor_git_dir),
                         msg='openstack/nova should be cloned.')
 
         jobdir_git_dir = os.path.join(build.jobdir.src_root,

tests/unit/test_webapp.py
@@ -27,7 +27,7 @@ class TestWebapp(ZuulTestCase):
 
     def setUp(self):
         super(TestWebapp, self).setUp()
-        self.launch_server.hold_jobs_in_build = True
+        self.execute_server.hold_jobs_in_build = True
         A = self.fake_gerrit.addFakeChange('org/project', 'master', 'A')
         A.addApproval('code-review', 2)
         self.fake_gerrit.addEvent(A.addApproval('approved', 1))
@@ -38,8 +38,8 @@ class TestWebapp(ZuulTestCase):
         self.port = self.webapp.server.socket.getsockname()[1]
 
     def tearDown(self):
-        self.launch_server.hold_jobs_in_build = False
-        self.launch_server.release()
+        self.execute_server.hold_jobs_in_build = False
+        self.execute_server.release()
         self.waitUntilSettled()
         super(TestWebapp, self).tearDown()
 

tests/unit/test_zuultrigger.py
@@ -27,7 +27,7 @@ class TestZuulTriggerParentChangeEnqueued(ZuulTestCase):
         # When A is enqueued in the gate, B1 and B2 should both attempt
         # to be enqueued in both pipelines.  B1 should end up in check
         # and B2 in gate because of differing pipeline requirements.
-        self.launch_server.hold_jobs_in_build = True
+        self.execute_server.hold_jobs_in_build = True
         A = self.fake_gerrit.addFakeChange('org/project', 'master', 'A')
         B1 = self.fake_gerrit.addFakeChange('org/project', 'master', 'B1')
         B2 = self.fake_gerrit.addFakeChange('org/project', 'master', 'B2')
@@ -46,8 +46,8 @@ class TestZuulTriggerParentChangeEnqueued(ZuulTestCase):
         # to enqueue behind 1,1 so that the test is more
         # deterministic.
         self.waitUntilSettled()
-        self.launch_server.hold_jobs_in_build = False
-        self.launch_server.release()
+        self.execute_server.hold_jobs_in_build = False
+        self.execute_server.release()
         self.waitUntilSettled()
 
         self.assertEqual(len(self.history), 3)

zuul/executor/client.py
@@ -102,7 +102,7 @@ def getJobData(job):
 
 class ZuulGearmanClient(gear.Client):
     def __init__(self, zuul_gearman):
-        super(ZuulGearmanClient, self).__init__('Zuul Launch Client')
+        super(ZuulGearmanClient, self).__init__('Zuul Execute Client')
         self.__zuul_gearman = zuul_gearman
 
     def handleWorkComplete(self, packet):
@@ -144,8 +144,8 @@ class ZuulGearmanClient(gear.Client):
                     self.__zuul_gearman.onUnknownJob(job)
 
 
-class LaunchClient(object):
-    log = logging.getLogger("zuul.LaunchClient")
+class ExecuteClient(object):
+    log = logging.getLogger("zuul.ExecuteClient")
     negative_function_cache_ttl = 5
 
     def __init__(self, config, sched):
@@ -209,10 +209,10 @@ class LaunchClient(object):
         self.log.debug("Function %s is not registered" % name)
         return False
 
-    def launch(self, job, item, pipeline, dependent_items=[]):
+    def execute(self, job, item, pipeline, dependent_items=[]):
         uuid = str(uuid4().hex)
         self.log.info(
-            "Launch job %s (uuid: %s) on nodes %s for change %s "
+            "Execute job %s (uuid: %s) on nodes %s for change %s "
             "with dependent changes %s" % (
                 job, uuid,
                 item.current_build_set.getJobNodeSet(job.name),
@@ -339,7 +339,7 @@ class LaunchClient(object):
             self.sched.onBuildCompleted(build, 'SUCCESS')
             return build
 
-        gearman_job = gear.Job('launcher:launch', json.dumps(params),
+        gearman_job = gear.Job('executor:execute', json.dumps(params),
                                unique=uuid)
         build.__gearman_job = gearman_job
         build.__gearman_manager = None
@@ -433,7 +433,7 @@ class LaunchClient(object):
             # internal dict after it's added to the report queue.
             del self.builds[job.unique]
         else:
-            if not job.name.startswith("launcher:stop:"):
+            if not job.name.startswith("executor:stop:"):
                 self.log.error("Unable to find build %s" % job.unique)
 
     def onWorkStatus(self, job):
@@ -483,7 +483,7 @@ class LaunchClient(object):
                            (build,))
         stop_uuid = str(uuid4().hex)
         data = dict(uuid=build.__gearman_job.unique)
-        stop_job = gear.Job("launcher:stop:%s" % build.__gearman_manager,
+        stop_job = gear.Job("executor:stop:%s" % build.__gearman_manager,
                             json.dumps(data), unique=stop_uuid)
         self.meta_jobs[stop_uuid] = stop_job
         self.log.debug("Submitting stop job: %s", stop_job)

openstack-infra/zuul

tests/unit/test_connection.py
@@ -46,7 +46,7 @@ class TestConnections(ZuulTestCase):
                          'jenkins')
 
         B = self.fake_review_gerrit.addFakeChange('org/project', 'master', 'B')
-        self.launch_server.failJob('project-test2', B)
+        self.executor_server.failJob('project-test2', B)
         self.addEvent('review_gerrit', B.getPatchsetCreatedEvent(1))
 
         self.waitUntilSettled()
@@ -239,7 +239,7 @@ class TestMultipleGerrits(ZuulTestCase):
     tenant_config_file = 'config/zuul-connections-multiple-gerrits/main.yaml'
 
     def test_multiple_project_separate_gerrits(self):
-        self.launch_server.hold_jobs_in_build = True
+        self.executor_server.hold_jobs_in_build = True
 
         A = self.fake_another_gerrit.addFakeChange(
             'org/project1', 'master', 'A')
@@ -276,6 +276,6 @@ class TestMultipleGerrits(ZuulTestCase):
                  pipeline='review_check'),
         ])
 
-        self.launch_server.hold_jobs_in_build = False
-        self.launch_server.release()
+        self.executor_server.hold_jobs_in_build = False
+        self.executor_server.release()
         self.waitUntilSettled()

tests/unit/test_openstack.py
@@ -58,7 +58,7 @@ class TestOpenStack(AnsibleZuulTestCase):
                          'ubuntu-trusty')
 
     def test_dsvm_keystone_repo(self):
-        self.launch_server.keep_jobdir = True
+        self.executor_server.keep_jobdir = True
         A = self.fake_gerrit.addFakeChange('openstack/nova', 'master', 'A')
         self.fake_gerrit.addEvent(A.getPatchsetCreatedEvent(1))
         self.waitUntilSettled()
@@ -68,9 +68,9 @@ class TestOpenStack(AnsibleZuulTestCase):
         build = self.getJobFromHistory('dsvm')
 
         # Check that a change to nova triggered a keystone clone
-        launcher_git_dir = os.path.join(self.launcher_src_root,
+        executor_git_dir = os.path.join(self.executor_src_root,
                                         'openstack', 'keystone', '.git')
-        self.assertTrue(os.path.exists(launcher_git_dir),
+        self.assertTrue(os.path.exists(executor_git_dir),
                         msg='openstack/keystone should be cloned.')
 
         jobdir_git_dir = os.path.join(build.jobdir.src_root,
@@ -79,7 +79,7 @@ class TestOpenStack(AnsibleZuulTestCase):
                         msg='openstack/keystone should be cloned.')
 
     def test_dsvm_nova_repo(self):
-        self.launch_server.keep_jobdir = True
+        self.executor_server.keep_jobdir = True
         A = self.fake_gerrit.addFakeChange('openstack/keystone', 'master', 'A')
         self.fake_gerrit.addEvent(A.getPatchsetCreatedEvent(1))
         self.waitUntilSettled()
@@ -89,9 +89,9 @@ class TestOpenStack(AnsibleZuulTestCase):
         build = self.getJobFromHistory('dsvm')
 
         # Check that a change to keystone triggered a nova clone
-        launcher_git_dir = os.path.join(self.launcher_src_root,
+        executor_git_dir = os.path.join(self.executor_src_root,
                                         'openstack', 'nova', '.git')
-        self.assertTrue(os.path.exists(launcher_git_dir),
+        self.assertTrue(os.path.exists(executor_git_dir),
                         msg='openstack/nova should be cloned.')
 
         jobdir_git_dir = os.path.join(build.jobdir.src_root,

tests/unit/test_webapp.py
@@ -27,7 +27,7 @@ class TestWebapp(ZuulTestCase):
 
     def setUp(self):
         super(TestWebapp, self).setUp()
-        self.launch_server.hold_jobs_in_build = True
+        self.executor_server.hold_jobs_in_build = True
         A = self.fake_gerrit.addFakeChange('org/project', 'master', 'A')
         A.addApproval('code-review', 2)
         self.fake_gerrit.addEvent(A.addApproval('approved', 1))
@@ -38,8 +38,8 @@ class TestWebapp(ZuulTestCase):
         self.port = self.webapp.server.socket.getsockname()[1]
 
     def tearDown(self):
-        self.launch_server.hold_jobs_in_build = False
-        self.launch_server.release()
+        self.executor_server.hold_jobs_in_build = False
+        self.executor_server.release()
         self.waitUntilSettled()
         super(TestWebapp, self).tearDown()
 

tests/unit/test_zuultrigger.py
@@ -27,7 +27,7 @@ class TestZuulTriggerParentChangeEnqueued(ZuulTestCase):
         # When A is enqueued in the gate, B1 and B2 should both attempt
         # to be enqueued in both pipelines.  B1 should end up in check
         # and B2 in gate because of differing pipeline requirements.
-        self.launch_server.hold_jobs_in_build = True
+        self.executor_server.hold_jobs_in_build = True
         A = self.fake_gerrit.addFakeChange('org/project', 'master', 'A')
         B1 = self.fake_gerrit.addFakeChange('org/project', 'master', 'B1')
         B2 = self.fake_gerrit.addFakeChange('org/project', 'master', 'B2')
@@ -46,8 +46,8 @@ class TestZuulTriggerParentChangeEnqueued(ZuulTestCase):
         # to enqueue behind 1,1 so that the test is more
         # deterministic.
         self.waitUntilSettled()
-        self.launch_server.hold_jobs_in_build = False
-        self.launch_server.release()
+        self.executor_server.hold_jobs_in_build = False
+        self.executor_server.release()
         self.waitUntilSettled()
 
         self.assertEqual(len(self.history), 3)

zuul/executor/client.py
@@ -102,7 +102,7 @@ def getJobData(job):
 
 class ZuulGearmanClient(gear.Client):
     def __init__(self, zuul_gearman):
-        super(ZuulGearmanClient, self).__init__('Zuul Launch Client')
+        super(ZuulGearmanClient, self).__init__('Zuul Executor Client')
         self.__zuul_gearman = zuul_gearman
 
     def handleWorkComplete(self, packet):
@@ -144,8 +144,8 @@ class ZuulGearmanClient(gear.Client):
                     self.__zuul_gearman.onUnknownJob(job)
 
 
-class LaunchClient(object):
-    log = logging.getLogger("zuul.LaunchClient")
+class ExecutorClient(object):
+    log = logging.getLogger("zuul.ExecutorClient")
     negative_function_cache_ttl = 5
 
     def __init__(self, config, sched):
@@ -209,10 +209,10 @@ class LaunchClient(object):
         self.log.debug("Function %s is not registered" % name)
         return False
 
-    def launch(self, job, item, pipeline, dependent_items=[]):
+    def execute(self, job, item, pipeline, dependent_items=[]):
         uuid = str(uuid4().hex)
         self.log.info(
-            "Launch job %s (uuid: %s) on nodes %s for change %s "
+            "Execute job %s (uuid: %s) on nodes %s for change %s "
             "with dependent changes %s" % (
                 job, uuid,
                 item.current_build_set.getJobNodeSet(job.name),
@@ -339,7 +339,7 @@ class LaunchClient(object):
             self.sched.onBuildCompleted(build, 'SUCCESS')
             return build
 
-        gearman_job = gear.Job('launcher:launch', json.dumps(params),
+        gearman_job = gear.Job('executor:execute', json.dumps(params),
                                unique=uuid)
         build.__gearman_job = gearman_job
         build.__gearman_manager = None
@@ -433,7 +433,7 @@ class LaunchClient(object):
             # internal dict after it's added to the report queue.
             del self.builds[job.unique]
         else:
-            if not job.name.startswith("launcher:stop:"):
+            if not job.name.startswith("executor:stop:"):
                 self.log.error("Unable to find build %s" % job.unique)
 
     def onWorkStatus(self, job):
@@ -483,7 +483,7 @@ class LaunchClient(object):
                            (build,))
         stop_uuid = str(uuid4().hex)
         data = dict(uuid=build.__gearman_job.unique)
-        stop_job = gear.Job("launcher:stop:%s" % build.__gearman_manager,
+        stop_job = gear.Job("executor:stop:%s" % build.__gearman_manager,
                             json.dumps(data), unique=stop_uuid)
         self.meta_jobs[stop_uuid] = stop_job
         self.log.debug("Submitting stop job: %s", stop_job)