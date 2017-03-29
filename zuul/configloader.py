blurrycontour/zuul-gerrit

Augment references of pkcs1 with oaep
Rightly the system in use is RSAES-OAEP, part of the PKCS#1 standard.
"PKCS#1" is not enough information to communicate to someone the
encryption scheme in use.  Refer to the scheme Zuul uses as "PKCS#1-OAEP"
or "pkcs1-oaep" to clarify.

Change-Id: I74dcde6fa3756354ce65233c64c6189d1b241e90

zuul/configloader.py
@@ -123,8 +123,8 @@ repo {repo} on branch {branch}.  The error was:
         loader.dispose()
 
 
-class EncryptedPKCS1(yaml.YAMLObject):
-    yaml_tag = u'!encrypted/pkcs1'
+class EncryptedPKCS1_OAEP(yaml.YAMLObject):
+    yaml_tag = u'!encrypted/pkcs1-oaep'
     yaml_loader = yaml.SafeLoader
 
     def __init__(self, data):
@@ -134,7 +134,7 @@ class EncryptedPKCS1(yaml.YAMLObject):
         return not self.__eq__(other)
 
     def __eq__(self, other):
-        if not isinstance(other, EncryptedPKCS1):
+        if not isinstance(other, EncryptedPKCS1_OAEP):
             return False
         return (self.data == other.data)
 
@@ -143,7 +143,7 @@ class EncryptedPKCS1(yaml.YAMLObject):
         return cls(node.value)
 
     def decrypt(self, private_key):
-        return encryption.decrypt_pkcs1(self.data, private_key)
+        return encryption.decrypt_pkcs1_oaep(self.data, private_key)
 
 
 class NodeSetParser(object):
@@ -175,7 +175,7 @@ class NodeSetParser(object):
 class SecretParser(object):
     @staticmethod
     def getSchema():
-        data = {str: vs.Any(str, EncryptedPKCS1)}
+        data = {str: vs.Any(str, EncryptedPKCS1_OAEP)}
 
         secret = {vs.Required('name'): str,
                   vs.Required('data'): data,

openstack-infra/zuul

zuul/configloader.py
@@ -123,8 +123,8 @@ repo {repo} on branch {branch}.  The error was:
         loader.dispose()
 
 
-class EncryptedPKCS1(yaml.YAMLObject):
-    yaml_tag = u'!encrypted/pkcs1'
+class EncryptedPKCS1_OAEP(yaml.YAMLObject):
+    yaml_tag = u'!encrypted/pkcs1-oaep'
     yaml_loader = yaml.SafeLoader
 
     def __init__(self, ciphertext):
@@ -134,7 +134,7 @@ class EncryptedPKCS1(yaml.YAMLObject):
         return not self.__eq__(other)
 
     def __eq__(self, other):
-        if not isinstance(other, EncryptedPKCS1):
+        if not isinstance(other, EncryptedPKCS1_OAEP):
             return False
         return (self.ciphertext == other.ciphertext)
 
@@ -143,7 +143,7 @@ class EncryptedPKCS1(yaml.YAMLObject):
         return cls(node.value)
 
     def decrypt(self, private_key):
-        return encryption.decrypt_pkcs1(self.ciphertext, private_key)
+        return encryption.decrypt_pkcs1_oaep(self.ciphertext, private_key)
 
 
 class NodeSetParser(object):
@@ -175,7 +175,7 @@ class NodeSetParser(object):
 class SecretParser(object):
     @staticmethod
     def getSchema():
-        data = {str: vs.Any(str, EncryptedPKCS1)}
+        data = {str: vs.Any(str, EncryptedPKCS1_OAEP)}
 
         secret = {vs.Required('name'): str,
                   vs.Required('data'): data,