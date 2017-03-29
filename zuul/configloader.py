blurrycontour/zuul-gerrit

Decrypt secrets and plumb to Ansible
When configuring jobs, decrypt secrets they reference.  Pass the
resulting values to ansible variables.

Change-Id: Ibe2b6b84fdc0f4287e4dc1681218df2228f92db0

zuul/configloader.py
@@ -24,6 +24,8 @@ import voluptuous as vs
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 from zuul import model
 import zuul.manager.dependent
 import zuul.manager.independent
@@ -130,11 +132,31 @@ class EncryptedPKCS1(yaml.YAMLObject):
     yaml_loader = yaml.SafeLoader
 
     def __init__(self, data):
-        self.data = data
+        self.data = data.decode('base64')
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __eq__(self, other):
+        if not isinstance(other, EncryptedPKCS1):
+            return False
+        return (self.data == other.data)
 
     @classmethod
     def from_yaml(cls, loader, node):
         return cls(node.value)
+
+    def decrypt(self, private_key):
+        # https://cryptography.io/en/latest/hazmat/primitives/asymmetric/rsa/#decryption
+        plaintext = private_key.decrypt(
+            self.data,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA1()),
+                algorithm=hashes.SHA1(),
+                label=None
+            )
+        )
+        return plaintext
 
 
 class NodeSetParser(object):
@@ -180,7 +202,7 @@ class SecretParser(object):
     def fromYaml(layout, conf):
         with configuration_exceptions('secret', conf):
             SecretParser.getSchema()(conf)
-        s = model.Secret(conf['name'])
+        s = model.Secret(conf['name'], conf['_source_context'])
         s.data = conf['data']
         return s
 
@@ -262,17 +284,17 @@ class JobParser(object):
         if 'auth' in conf:
             job.auth = model.AuthContext()
             if 'inherit' in conf['auth']:
-                auth.inherit = conf['auth']['inherit']
+                job.auth.inherit = conf['auth']['inherit']
 
-            for conf_secret in conf['auth'].get('secrets', []):
-                secret_name = conf_secret['name']
+            for secret_name in conf['auth'].get('secrets', []):
                 secret = layout.secrets[secret_name]
                 if secret.source_context != job.source_context:
                     raise Exception(
                         "Unable to use secret %s.  Secrets must be "
                         "defined in the same project in which they "
                         "are used" % secret_name)
-                auth.secrets.append(secret)
+                job.auth.secrets.append(secret.decrypt(
+                    job.source_context.project.private_key))
 
         if 'parent' in conf:
             parent = layout.getJob(conf['parent'])

zuul/executor/client.py
@@ -315,6 +315,9 @@ class ExecutorClient(object):
                               public_ipv4=node.public_ipv4))
         params['nodes'] = nodes
         params['vars'] = copy.deepcopy(job.variables)
+        if job.auth:
+            for secret in job.auth.secrets:
+                params['vars'][secret.name] = copy.deepcopy(secret.data)
         params['vars']['zuul'] = zuul_params
         projects = set()
         if job.repos:

openstack-infra/zuul

zuul/configloader.py
@@ -24,6 +24,8 @@ import voluptuous as vs
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 from zuul import model
 import zuul.manager.dependent
 import zuul.manager.independent
@@ -130,11 +132,31 @@ class EncryptedPKCS1(yaml.YAMLObject):
     yaml_loader = yaml.SafeLoader
 
     def __init__(self, ciphertext):
-        self.ciphertext = ciphertext
+        self.ciphertext = ciphertext.decode('base64')
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __eq__(self, other):
+        if not isinstance(other, EncryptedPKCS1):
+            return False
+        return (self.ciphertext == other.ciphertext)
 
     @classmethod
     def from_yaml(cls, loader, node):
         return cls(node.value)
+
+    def decrypt(self, private_key):
+        # https://cryptography.io/en/latest/hazmat/primitives/asymmetric/rsa/#decryption
+        plaintext = private_key.decrypt(
+            self.ciphertext,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA1()),
+                algorithm=hashes.SHA1(),
+                label=None
+            )
+        )
+        return plaintext
 
 
 class NodeSetParser(object):
@@ -272,7 +294,8 @@ class JobParser(object):
                         "Unable to use secret %s.  Secrets must be "
                         "defined in the same project in which they "
                         "are used" % secret_name)
-                job.auth.secrets.append(secret)
+                job.auth.secrets.append(secret.decrypt(
+                    job.source_context.project.private_key))
 
         if 'parent' in conf:
             parent = layout.getJob(conf['parent'])

zuul/executor/client.py
@@ -318,6 +318,9 @@ class ExecutorClient(object):
                               public_ipv4=node.public_ipv4))
         params['nodes'] = nodes
         params['vars'] = copy.deepcopy(job.variables)
+        if job.auth:
+            for secret in job.auth.secrets:
+                params['vars'][secret.name] = copy.deepcopy(secret.secret_data)
         params['vars']['zuul'] = zuul_params
         projects = set()
         if job.repos: