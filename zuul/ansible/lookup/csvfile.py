abbaa6f2c60396bc81e80b60994cbfbd5e6cd1f6
James E. Blair <jeblair@redhat.com>
2017-04-06 16:11:44 -0700

    Add some ansible plugin tests
    This is not complete, but it's a start.
    
    While doing this it became clear that the upstream csvfile lookup plugin does
    not work in python3. Work around that by putting in a simpler version of
    the code into our copy.
    
    Change-Id: Ic84d8265e6fd7e15a0e5d66c781409a087d761d7

--- zuul/ansible/lookup/csvfile.py
+++ zuul/ansible/lookup/csvfile.py
@@ -13,6 +13,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>.
 
+import csv
+
+from ansible.errors import AnsibleError
+from ansible.module_utils._text import to_native
 
 from zuul.ansible import paths
 csvfile = paths._import_ansible_lookup_plugin("csvfile")
@@ -20,6 +24,21 @@ csvfile = paths._import_ansible_lookup_plugin("csvfile")
 
 class LookupModule(csvfile.LookupModule):
 
-    def read_csv(self, filename, *args, **kwargs):
+    def read_csv(
+            self, filename, key, delimiter, encoding='utf-8',
+            dflt=None, col=1):
         paths._fail_if_unsafe(filename)
-        return super(LookupModule, self).read_csv(filename, *args, **kwargs)
+
+        # upstream csvfile read_csv does not work with python3 so
+        # carry our own version.
+        try:
+            f = open(filename, 'r')
+            creader = csv.reader(f, dialect=csv.excel, delimiter=delimiter)
+
+            for row in creader:
+                if row[0] == key:
+                    return row[int(col)]
+        except Exception as e:
+            raise AnsibleError("csvfile: %s" % to_native(e))
+
+        return dflt
