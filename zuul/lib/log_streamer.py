4dfcf54336ff71af20f77b184dd11a9d020a5eb8
David Shrewsbury <shrewsbury.dave@gmail.com>
2017-06-08 13:22:12 -0400

    Correct location of ansible log file
    The ansible output file will be under UUID/work/logs directory.
    
    Change-Id: Ia2d86e5ad8d9984a9c9ef378a9cfb941a96546f4

--- zuul/lib/log_streamer.py
+++ zuul/lib/log_streamer.py
@@ -95,7 +95,7 @@ class RequestHandler(ss.BaseRequestHandler):
             return
 
         # check if log file exists
-        log_file = os.path.join(job_dir, 'ansible', 'job-output.txt')
+        log_file = os.path.join(job_dir, 'work', 'logs', 'job-output.txt')
         if not os.path.exists(log_file):
             msg = 'Log not found for build ID %s' % build_uuid
             self.request.sendall(msg.encode("utf-8"))
