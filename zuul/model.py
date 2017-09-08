c5043212ea2221626d2b076291c75a3c69a02b81
Tobias Henkel <tobias.henkel@bmw.de>
2017-09-08 08:53:47 +0200

    Use connection type supplied from nodepool
    For supporting windows nodes we need the connection type to be
    configurable. This adds the ansible_connection host variable if
    nodepool defines it.
    
    Change-Id: I6d2f81c7586ae0d533add95ea96a9ea8ce8c3ab5

--- zuul/model.py
+++ zuul/model.py
@@ -384,6 +384,7 @@ class Node(object):
         self.private_ipv4 = None
         self.public_ipv6 = None
         self.connection_port = 22
+        self.connection_type = None
         self._keys = []
         self.az = None
         self.provider = None
