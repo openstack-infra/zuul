8fdef7433310388ee3252279704be3b1bd9ad914
James E. Blair <jeblair@redhat.com>
2017-10-10 11:15:06 -0700

    Add comment explaining gitpython requirement
    Also clean up some tabs.
    
    Change-Id: If641a164c21dc7b13d48548558ea16e0c0a0b400

--- tests/fixtures/fake_git.sh
+++ tests/fixtures/fake_git.sh
@@ -3,12 +3,12 @@
 echo $*
 case "$1" in
     clone)
-	dest=$3
-	mkdir -p $dest/.git
-	;;
+        dest=$3
+        mkdir -p $dest/.git
+        ;;
     version)
-	echo "git version 1.0.0"
-	exit 0
-	;;
+        echo "git version 1.0.0"
+        exit 0
+        ;;
 esac
 sleep 30
