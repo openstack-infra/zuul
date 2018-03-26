ddbe62abb2cf988a6dd18690c8a2747752dc5115
Tristan Cacqueray <tdecacqu@redhat.com>
2018-03-26 00:18:37 +0000

    autohold: prevent using ',' in change argument
    The autohold command doesn't support patchset number and the list command
    fails with ValueError. This change prevents adding an autohold on a
    fully described change,patchset gerrit change.
    
    Change-Id: I1be7e5c90526bab7e5eefe715bb103ff3a9b2584

--- zuul/cmd/client.py
+++ zuul/cmd/client.py
@@ -181,6 +181,9 @@ class Client(zuul.cmd.ZuulApp):
         if self.args.change and self.args.ref:
             print("Change and ref can't be both used for the same request")
             return False
+        if "," in self.args.change:
+            print("Error: change argument can not contain any ','")
+            return False
 
         r = client.autohold(tenant=self.args.tenant,
                             project=self.args.project,
