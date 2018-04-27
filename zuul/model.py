blurrycontour/zuul-gerrit

Add regex support to project stanzas
This adds support for regex matches of project stanzas such we can add
the same jobs to a large number of projects at once without having to
add each of them like in the following project stanza.

- job:
    name: project-common-test

- job:
    name: project-test1

- job:
    name: project-test2

- project:
    regex: org/project.+
    check:
      jobs:
        - project-common-test

- project:
    name: org/project1
    check:
      jobs:
        - project-test1

- project:
    name: org/project2
    check:
      jobs:
        - project-test2

Change-Id: I3d9a1f22a4659c9b0c63a320798a9f19c95cc79a

zuul/model.py
@@ -17,6 +17,7 @@ from collections import OrderedDict
 import copy
 import logging
 import os
+import re
 import struct
 import time
 from uuid import uuid4
@@ -2270,6 +2271,7 @@ class UnparsedTenantConfig(object):
         self.jobs = []
         self.project_templates = []
         self.projects = {}
+        self.projects_by_regex = {}
         self.nodesets = []
         self.secrets = []
         self.semaphores = []
@@ -2280,6 +2282,7 @@ class UnparsedTenantConfig(object):
         r.jobs = copy.deepcopy(self.jobs)
         r.project_templates = copy.deepcopy(self.project_templates)
         r.projects = copy.deepcopy(self.projects)
+        r.projects_by_regex = copy.deepcopy(self.projects_by_regex)
         r.nodesets = copy.deepcopy(self.nodesets)
         r.secrets = copy.deepcopy(self.secrets)
         r.semaphores = copy.deepcopy(self.semaphores)
@@ -2303,6 +2306,8 @@ class UnparsedTenantConfig(object):
                     if project is not None:
                         name = project.canonical_name
                 self.projects.setdefault(name, []).extend(v)
+            for k, v in conf.projects_by_regex.items():
+                self.projects_by_regex.setdefault(k, []).extend(v)
             self.nodesets.extend(conf.nodesets)
             self.secrets.extend(conf.secrets)
             self.semaphores.extend(conf.semaphores)
@@ -2318,8 +2323,17 @@ class UnparsedTenantConfig(object):
                 raise ConfigItemMultipleKeysError()
             key, value = list(item.items())[0]
             if key == 'project':
-                name = value['name']
-                self.projects.setdefault(name, []).append(value)
+                name = value.get('name')
+                regex = value.get('regex')
+                if name:
+                    self.projects.setdefault(name, []).append(value)
+                elif regex:
+                    self.projects_by_regex.setdefault(regex, []).append(value)
+                else:
+                    # Neither name nor regex was given. In this case add this
+                    # to some dummy project such that voluptuous can give a
+                    # better error message to the user.
+                    self.projects.setdefault('###invalid###', []).append(value)
             elif key == 'job':
                 self.jobs.append(value)
             elif key == 'project-template':
@@ -2665,6 +2679,46 @@ class Tenant(object):
         # This should never happen:
         raise Exception("Project %s is neither trusted nor untrusted" %
                         (project,))
+
+    def getProjectsByRegex(self, regex):
+        """Return all projects with a full match to either project name or
+        canonical project name.
+
+        :arg str regex: The regex to match
+        :returns: A list of tuples (trusted, project) describing the found
+            projects. Raises an exception if the same project name is found
+            several times across multiple hostnames.
+        """
+
+        matcher = re.compile(regex)
+        projects = []
+        result = []
+
+        for name, hostname_dict in self.projects.items():
+
+            if matcher.fullmatch(name):
+                # validate that this match is unambiguous
+                values = list(hostname_dict.values())
+                if len(values) > 1:
+                    raise Exception("Project name '%s' is ambiguous, "
+                                    "please fully qualify the project "
+                                    "with a hostname" % (name,))
+                projects.append(values[0])
+            else:
+                # try to match canonical project names
+                for project in hostname_dict.values():
+                    if matcher.fullmatch(project.canonical_name):
+                        project.append(project)
+
+        for project in projects:
+            if project in self.config_projects:
+                result.append((True, project))
+            elif project in self.untrusted_projects:
+                result.append((False, project))
+            else:
+                raise Exception("Project %s is neither trusted nor untrusted" %
+                                (project,))
+        return result
 
     def addConfigProject(self, tpc):
         self.config_projects.append(tpc.project)

openstack-infra/zuul

    name: ^org/project.+

zuul/model.py
@@ -17,6 +17,7 @@ from collections import OrderedDict
 import copy
 import logging
 import os
+import re2
 import struct
 import time
 from uuid import uuid4
@@ -2733,6 +2734,7 @@ class ParsedConfig(object):
         self.jobs = []
         self.project_templates = []
         self.projects = []
+        self.projects_by_regex = {}
         self.nodesets = []
         self.secrets = []
         self.semaphores = []
@@ -2744,6 +2746,7 @@ class ParsedConfig(object):
         r.jobs = self.jobs[:]
         r.project_templates = self.project_templates[:]
         r.projects = self.projects[:]
+        r.projects_by_regex = copy.copy(self.projects_by_regex)
         r.nodesets = self.nodesets[:]
         r.secrets = self.secrets[:]
         r.semaphores = self.semaphores[:]
@@ -2759,6 +2762,8 @@ class ParsedConfig(object):
             self.nodesets.extend(conf.nodesets)
             self.secrets.extend(conf.secrets)
             self.semaphores.extend(conf.semaphores)
+            for regex, projects in conf.projects_by_regex.items():
+                self.projects_by_regex.setdefault(regex, []).extend(projects)
             return
         else:
             raise ConfigItemUnknownError()
@@ -3341,6 +3346,47 @@ class Tenant(object):
         # This should never happen:
         raise Exception("Project %s is neither trusted nor untrusted" %
                         (project,))
+
+    def getProjectsByRegex(self, regex):
+        """Return all projects with a full match to either project name or
+        canonical project name.
+
+        :arg str regex: The regex to match
+        :returns: A list of tuples (trusted, project) describing the found
+            projects. Raises an exception if the same project name is found
+            several times across multiple hostnames.
+        """
+
+        matcher = re2.compile(regex)
+        projects = []
+        result = []
+
+        for name, hostname_dict in self.projects.items():
+
+            if matcher.fullmatch(name):
+                # validate that this match is unambiguous
+                values = list(hostname_dict.values())
+                if len(values) > 1:
+                    raise Exception("Project name '%s' is ambiguous, "
+                                    "please fully qualify the project "
+                                    "with a hostname. Valid hostnames "
+                                    "are %s." % (name, hostname_dict.keys()))
+                projects.append(values[0])
+            else:
+                # try to match canonical project names
+                for project in hostname_dict.values():
+                    if matcher.fullmatch(project.canonical_name):
+                        projects.append(project)
+
+        for project in projects:
+            if project in self.config_projects:
+                result.append((True, project))
+            elif project in self.untrusted_projects:
+                result.append((False, project))
+            else:
+                raise Exception("Project %s is neither trusted nor untrusted" %
+                                (project,))
+        return result
 
     def getProjectBranches(self, project):
         """Return a project's branches (filtered by this tenant config)