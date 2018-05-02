9d6c0a9307a55bda5679b897c99638cd6841cb39
Paul Belanger <pabelanger@redhat.com>
2018-05-02 15:26:43 -0400

    Add tox-py36 jobs
    Now that we have ubuntu-bionic images online, we can start gating for
    python3.6.
    
    We only need to install bubblewrap PPA for ubuntu-xenial,
    ubuntu-bionic already has bubblewrap packaged.
    
    Update bindep.txt skipping ubuntu-xenial for bubblewrap.
    
    Change-Id: Ic850a07001a9adef9cc459fcb107e0ebaaddf8ab
    Depends-On: https://review.openstack.org/565884
    Depends-On: https://review.openstack.org/565948
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -42,12 +42,14 @@ mysql -u $DB_USER -p$DB_PW -h 127.0.0.1 -e "
 sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN SUPERUSER PASSWORD '$DB_PW';"
 sudo -u postgres psql -c "CREATE DATABASE openstack_citest OWNER $DB_USER TEMPLATE template0 ENCODING 'UTF8';"
 
-# TODO(pabelanger): Move this into bindep after we figure out how to enable our
-# PPA.
-# NOTE(pabelanger): Avoid hitting http://keyserver.ubuntu.com
-sudo apt-key add $TOOLSDIR/018D05F5.gpg
 LSBDISTCODENAME=$(lsb_release -cs)
-echo "deb http://ppa.launchpad.net/openstack-ci-core/bubblewrap/ubuntu $LSBDISTCODENAME main" | \
-    sudo tee /etc/apt/sources.list.d/openstack-ci-core-ubuntu-bubblewrap-xenial.list
-sudo apt-get update
-sudo apt-get --assume-yes install bubblewrap
+if [ $LSBDISTCODENAME == 'xenial' ]; then
+    # TODO(pabelanger): Move this into bindep after we figure out how to enable our
+    # PPA.
+    # NOTE(pabelanger): Avoid hitting http://keyserver.ubuntu.com
+    sudo apt-key add $TOOLSDIR/018D05F5.gpg
+    echo "deb http://ppa.launchpad.net/openstack-ci-core/bubblewrap/ubuntu $LSBDISTCODENAME main" | \
+        sudo tee /etc/apt/sources.list.d/openstack-ci-core-ubuntu-bubblewrap-xenial.list
+    sudo apt-get update
+    sudo apt-get --assume-yes install bubblewrap
+fi
