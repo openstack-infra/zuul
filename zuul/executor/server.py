blurrycontour/zuul-gerrit

Add support for Ansible extra-vars flag
Currently, variables using the extra-vars flags always win precedence
over any other variable in ansible. There is also a 2nd use case where
playbooks variables for serial, hosts, etc can only be set using
extra-vars CLI flag.

While this could be achieved by using secrets today, it doesn't feel
like the correct way to use them. Additionally, secrets are
dictionary values in ansible, making them hard to use the filters like
default().

Change-Id: I6d8018661f8d13b7324a012cdbf9614e983e5114
Signed-off-by: Paul Belanger <pabelanger@redhat.com>

zuul/executor/server.py
@@ -263,6 +263,7 @@ class JobDirPlaybook(object):
         self.roles = []
         self.roles_path = []
         self.ansible_config = os.path.join(self.root, 'ansible.cfg')
+        self.extra_vars = os.path.join(self.root, 'extra_vars.yaml')
         self.project_link = os.path.join(self.root, 'project')
         self.secrets_root = os.path.join(self.root, 'secrets')
         os.makedirs(self.secrets_root)
@@ -295,6 +296,7 @@ class JobDir(object):
         #     fact-cache/localhost
         #     cp
         #   playbook_0 (mounted in bwrap for each playbook read-only)
+        #     extra_vars.yaml
         #     secrets.yaml
         #     project -> ../trusted/project_0/...
         #     role_0 -> ../trusted/project_0/...
@@ -1115,6 +1117,10 @@ class AnsibleJob(object):
             jobdir_playbook.secrets_content = yaml.safe_dump(
                 secrets, default_flow_style=False)
 
+        with open(jobdir_playbook.extra_vars, 'w') as extra_vars:
+            extra_vars.write(
+                yaml.safe_dump(args['extra_vars'], default_flow_style=False))
+
         self.writeAnsibleConfig(jobdir_playbook)
 
     def checkoutTrustedProject(self, project, branch):
@@ -1577,6 +1583,10 @@ class AnsibleJob(object):
             verbose = '-v'
 
         cmd = ['ansible-playbook', verbose, playbook.path]
+        # NOTE(pabelanger): Order for multiple playbooks is important, with
+        # this setup, extra-vars take priority over secrets, with the same
+        # variable name.
+        cmd.extend(['-e', '@' + playbook.extra_vars])
         if playbook.secrets_content:
             cmd.extend(['-e', '@' + playbook.secrets])
 

openstack-infra/zuul

zuul/executor/server.py
@@ -296,6 +296,7 @@ class JobDir(object):
         #   ansible (mounted in bwrap read-only)
         #     logging.json
         #     inventory.yaml
+        #     extra_vars.yaml
         #   .ansible (mounted in bwrap read-write)
         #     fact-cache/localhost
         #     cp
@@ -367,6 +368,7 @@ class JobDir(object):
             pass
         self.known_hosts = os.path.join(ssh_dir, 'known_hosts')
         self.inventory = os.path.join(self.ansible_root, 'inventory.yaml')
+        self.extra_vars = os.path.join(self.ansible_root, 'extra_vars.yaml')
         self.setup_inventory = os.path.join(self.ansible_root,
                                             'setup-inventory.yaml')
         self.logging_json = os.path.join(self.ansible_root, 'logging.json')
@@ -1386,6 +1388,10 @@ class AnsibleJob(object):
                 for key in node['host_keys']:
                     known_hosts.write('%s\n' % key)
 
+        with open(self.jobdir.extra_vars, 'w') as extra_vars:
+            extra_vars.write(
+                yaml.safe_dump(args['extra_vars'], default_flow_style=False))
+
     def writeLoggingConfig(self):
         self.log.debug("Writing logging config for job %s %s",
                        self.jobdir.job_output_file,
@@ -1741,6 +1747,8 @@ class AnsibleJob(object):
         cmd = ['ansible-playbook', verbose, playbook.path]
         if playbook.secrets_content:
             cmd.extend(['-e', '@' + playbook.secrets])
+
+        cmd.extend(['-e', '@' + self.jobdir.extra_vars])
 
         if success is not None:
             cmd.extend(['-e', 'zuul_success=%s' % str(bool(success))])