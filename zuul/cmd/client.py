blurrycontour/zuul-gerrit

Add a dequeue command to zuul client
Add the ability for an operator to dequeue a change from a pipeline.

Change-Id: I4524291807c8b97b62cfaa31fb5d46dc48adbac9
Signed-off-by: Paul Belanger <pabelanger@redhat.com>

zuul/cmd/client.py
@@ -111,6 +111,16 @@ class Client(zuul.cmd.ZuulApp):
             '--newrev', help='new revision', default=None)
         cmd_enqueue.set_defaults(func=self.enqueue_ref)
 
+        cmd_dequeue = subparsers.add_parser('dequeue',
+                                            help='dequeue one or more changes')
+        cmd_dequeue.add_argument('--pipeline', help='pipeline name',
+                                 required=True)
+        cmd_dequeue.add_argument('--tenant', help='tenant name',
+                                 required=True)
+        cmd_dequeue.add_argument('--changes', help='change ids',
+                                 required=True, nargs='+')
+        cmd_dequeue.set_defaults(func=self.dequeue)
+
         cmd_promote = subparsers.add_parser('promote',
                                             help='promote one or more changes')
         cmd_promote.add_argument('--tenant', help='tenant name',
@@ -248,6 +258,13 @@ class Client(zuul.cmd.ZuulApp):
                                ref=self.args.ref,
                                oldrev=self.args.oldrev,
                                newrev=self.args.newrev)
+        return r
+
+    def dequeue(self):
+        client = zuul.rpcclient.RPCClient(self.server, self.port)
+        r = client.dequeue(tenant=self.args.tenant,
+                           pipeline=self.args.pipeline,
+                           change_ids=self.args.changes)
         return r
 
     def promote(self):

openstack-infra/zuul

zuul/cmd/client.py
@@ -111,6 +111,21 @@ class Client(zuul.cmd.ZuulApp):
             '--newrev', help='new revision', default=None)
         cmd_enqueue.set_defaults(func=self.enqueue_ref)
 
+        cmd_dequeue = subparsers.add_parser('dequeue',
+                                            help='dequeue a buildset by its '
+                                                 'change or ref')
+        cmd_dequeue.add_argument('--tenant', help='tenant name',
+                                 required=True)
+        cmd_dequeue.add_argument('--pipeline', help='pipeline name',
+                                 required=True)
+        cmd_dequeue.add_argument('--project', help='project name',
+                                 required=True)
+        cmd_dequeue.add_argument('--change', help='change id',
+                                 default=None)
+        cmd_dequeue.add_argument('--ref', help='ref name',
+                                 default=None)
+        cmd_dequeue.set_defaults(func=self.dequeue)
+
         cmd_promote = subparsers.add_parser('promote',
                                             help='promote one or more changes')
         cmd_promote.add_argument('--tenant', help='tenant name',
@@ -164,6 +179,12 @@ class Client(zuul.cmd.ZuulApp):
                 self.args.oldrev = '0000000000000000000000000000000000000000'
             if self.args.newrev is None:
                 self.args.newrev = '0000000000000000000000000000000000000000'
+        if self.args.func == self.dequeue:
+            if self.args.change is None and self.args.ref is None:
+                parser.error("Change or ref needed.")
+            if self.args.change is not None and self.args.ref is not None:
+                parser.error(
+                    "The 'change' and 'ref' arguments are mutually exclusive.")
 
     def setup_logging(self):
         """Client logging does not rely on conf file"""
@@ -253,6 +274,16 @@ class Client(zuul.cmd.ZuulApp):
                                ref=self.args.ref,
                                oldrev=self.args.oldrev,
                                newrev=self.args.newrev)
+        return r
+
+    def dequeue(self):
+        client = zuul.rpcclient.RPCClient(
+            self.server, self.port, self.ssl_key, self.ssl_cert, self.ssl_ca)
+        r = client.dequeue(tenant=self.args.tenant,
+                           pipeline=self.args.pipeline,
+                           project=self.args.project,
+                           change=self.args.change,
+                           ref=self.args.ref)
         return r
 
     def promote(self):