blurrycontour/zuul-gerrit

web: rewrite interface in react
This change rewrites the web interface using React:
http://lists.zuul-ci.org/pipermail/zuul-discuss/2018-August/000528.html

Depends-On: https://review.openstack.org/591964
Change-Id: Ic6c33102ac3da69ebd0b8e9c6c8b431d51f3cfd4
Co-Authored-By: Monty Taylor <mordred@inaugust.com>
Co-Authored-By: James E. Blair <jeblair@redhat.com>

web/src/index.js
@@ -1,0 +1,39 @@
+// Copyright 2018 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+// The index is the main of the project. The App is wrapped with
+// a Provider to share the redux store and a Router to manage the location.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import 'patternfly/dist/css/patternfly.min.css'
+import 'patternfly/dist/css/patternfly-additions.min.css'
+import './index.css'
+
+import { getHomepageUrl } from './api'
+import registerServiceWorker from './registerServiceWorker'
+import { store, fetchInfoAction } from './reducers'
+import App from './App'
+
+// This calls the /api/info endpoint asynchronously, the App is connected
+// with redux and it will update the info prop when fetch succeed.
+store.dispatch(fetchInfoAction())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router basename={new URL(getHomepageUrl()).pathname}><App /></Router>
+  </Provider>, document.getElementById('root'))
+registerServiceWorker()

web/src/routes.js
@@ -1,0 +1,51 @@
+// Copyright 2018 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import Status from './pages/status'
+import Jobs from './pages/jobs'
+import Builds from './pages/builds'
+import Tenants from './pages/tenants'
+import Stream from './pages/stream'
+
+// The Route object are created in the App component.
+// Object with a title are created in the menu.
+// Object with globalRoute are not tenant scoped.
+const routes = () => [
+  {
+    title: 'Status',
+    to: '/status',
+    component: Status
+  },
+  {
+    title: 'Jobs',
+    to: '/jobs',
+    component: Jobs
+  },
+  {
+    title: 'Builds',
+    to: '/builds',
+    component: Builds
+  },
+  {
+    to: '/stream/:buildId',
+    component: Stream
+  },
+  {
+    to: '/tenants',
+    component: Tenants,
+    globalRoute: true
+  }
+]
+
+export { routes }

openstack-infra/zuul

web/src/index.js
@@ -1,0 +1,40 @@
+// Copyright 2018 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+// The index is the main of the project. The App is wrapped with
+// a Provider to share the redux store and a Router to manage the location.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import 'patternfly/dist/css/patternfly.min.css'
+import 'patternfly/dist/css/patternfly-additions.min.css'
+import './index.css'
+
+import { getHomepageUrl } from './api'
+import registerServiceWorker from './registerServiceWorker'
+import { createZuulStore, fetchInfoAction } from './reducers'
+import App from './App'
+
+// This calls the /api/info endpoint asynchronously, the App is connected
+// with redux and it will update the info prop when fetch succeed.
+const store = createZuulStore()
+store.dispatch(fetchInfoAction())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router basename={new URL(getHomepageUrl()).pathname}><App /></Router>
+  </Provider>, document.getElementById('root'))
+registerServiceWorker()

web/src/routes.js
@@ -1,0 +1,52 @@
+// Copyright 2018 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import StatusPage from './pages/Status'
+import JobsPage from './pages/Jobs'
+import BuildsPage from './pages/Builds'
+import TenantsPage from './pages/Tenants'
+import StreamPage from './pages/Stream'
+
+// The Route object are created in the App component.
+// Object with a title are created in the menu.
+// Object with globalRoute are not tenant scoped.
+// Remember to update the api getHomepageUrl subDir list for route with params
+const routes = () => [
+  {
+    title: 'Status',
+    to: '/status',
+    component: StatusPage
+  },
+  {
+    title: 'Jobs',
+    to: '/jobs',
+    component: JobsPage
+  },
+  {
+    title: 'Builds',
+    to: '/builds',
+    component: BuildsPage
+  },
+  {
+    to: '/stream/:buildId',
+    component: StreamPage
+  },
+  {
+    to: '/tenants',
+    component: TenantsPage,
+    globalRoute: true
+  }
+]
+
+export { routes }