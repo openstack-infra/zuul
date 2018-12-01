505c32e4b23ef81f7b4a13f91c6d062d3f1fa4fc
James E. Blair <jeblair@redhat.com>
2018-11-30 16:23:38 -0800

    Fix updating relative priority
    This code path was untested and had some typos.  Correct them and
    ensure the path is tested.
    
    Change-Id: Ib4a283f739b12295f480684b9b93ad8a60abf350

--- zuul/manager/__init__.py
+++ zuul/manager/__init__.py
@@ -88,7 +88,10 @@ class PipelineManager(object):
     def getNodePriority(self, item):
         items = self.pipeline.getAllItems()
         items = [i for i in items if i.change.project == item.change.project]
-        return items.index(item)
+        for idx, val in enumerate(items):
+            if item == val:
+                return idx
+        return len(items)
 
     def isChangeAlreadyInPipeline(self, change):
         # Checks live items in the pipeline
@@ -700,7 +703,7 @@ class PipelineManager(object):
             priority = item.getNodePriority()
             for node_request in item.current_build_set.node_requests.values():
                 if node_request.relative_priority != priority:
-                    self.sched.nodepool.reviseNodeRequest(
+                    self.sched.nodepool.reviseRequest(
                         node_request, priority)
         return (changed, nnfi)
 
