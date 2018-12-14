753d79c13a6a5d486ceb8bc693b9b5b348bf4c36
Tristan Cacqueray <tdecacqu@redhat.com>
2018-12-14 03:53:57 +0000

    web: refactor projects page to use a reducer
    This change updates the projects page to dispatch reducer action
    instead of direct axios call. This enables using the generic
    error reducers  as well as keeping the jobs list in the store to
    avoid repeated query.
    
    Change-Id: Ie9d5694649df515388f2f5e606887451ccdcf769

---
+++ web/src/reducers/projects.js
@@ -0,0 +1,47 @@
+// Copyright 2018 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import {
+  PROJECTS_FETCH_FAIL,
+  PROJECTS_FETCH_REQUEST,
+  PROJECTS_FETCH_SUCCESS
+} from '../actions/projects'
+
+import update from 'immutability-helper'
+
+export default (state = {
+  isFetching: false,
+  projects: {},
+}, action) => {
+  switch (action.type) {
+    case PROJECTS_FETCH_REQUEST:
+      return {
+        isFetching: true,
+        projects: state.projects,
+      }
+    case PROJECTS_FETCH_SUCCESS:
+      return {
+        isFetching: false,
+        projects: update(
+          state.projects, {$merge: {[action.tenant]: action.projects}}),
+      }
+    case PROJECTS_FETCH_FAIL:
+      return {
+        isFetching: false,
+        projects: state.projects,
+      }
+    default:
+      return state
+  }
+}
